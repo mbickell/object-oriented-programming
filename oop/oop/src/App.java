ceb2bd263de5493796605101741f99ab0c3a7e54
Matthew Bickell <matthewbickell@hotmail.com>
2026-02-16 12:58:52 +0200

    fix: rename project

--- oop/oop/src/App.java
+++ oop/oop/src/App.java
@@ -1,7 +1,7 @@
 
 public class App {
   public static void main(String[] args) throws Exception {
-    Garage garage = new Garage(10);
+    Garage garage = new Garage(50);
 
     garage.printCars();
 
