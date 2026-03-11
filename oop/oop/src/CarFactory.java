88f3c16387a347dfab8d80bb857d232ee40ae463
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-11 10:36:22 +0200

    feat: car factory now randomly picks a car to use

--- oop/oop/src/CarFactory.java
+++ oop/oop/src/CarFactory.java
@@ -4,19 +4,27 @@ public class CarFactory {
   private String[] models = { "Octavia", "Recon", "A5", "Rio", "GX", "Clio", "Enclave", "G90",
       "A7", "Hornet" };
 
-  public GasCar generateCar(String brand, String model, int tyreCount) {
+  public Car generateCar(String brand, String model, int tyreCount) {
     int gasLevel = Utils.generateRandomNumber(1, 100);
-    return new GasCar(gasLevel, brand, model, tyreCount);
+    int randomCarSelector = Utils.generateRandomNumber(1, 3);
+
+    switch (randomCarSelector) {
+      case 1:
+        return new GasCar(gasLevel, brand, model, tyreCount);
+      case 2:
+        return new HybridCar(gasLevel, brand, model, tyreCount);
+      case 3:
+        return new ElectricCar(gasLevel, brand, model, tyreCount);
+      default:
+        return new GasCar(gasLevel, brand, model, tyreCount);
+    }
   }
 
-  public GasCar generateCar() {
+  public Car generateCar() {
     int carBrandIndex = Utils.generateRandomNumber(0, 9);
     int tyreCount = Utils.generateRandomNumber(0, 4);
-    // EngineType[] engineTypeArr = EngineType.values();
-    // int engineTypeIndex = Utils.generateRandomNumber(0, engineTypeArr.length -
-    // 1);
 
-    GasCar car = this.generateCar(this.brands[carBrandIndex], this.models[carBrandIndex], tyreCount);
+    Car car = this.generateCar(this.brands[carBrandIndex], this.models[carBrandIndex], tyreCount);
     return car;
   }
 }
