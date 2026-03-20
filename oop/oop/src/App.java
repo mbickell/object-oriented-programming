937f0b2e37350761d8bf4b68e091280d3fc8a713
Matthew Bickell <matthewbickell@hotmail.com>
2026-03-20 10:12:16 +0200

    feat: clean app

--- oop/oop/src/App.java
+++ oop/oop/src/App.java
@@ -3,29 +3,24 @@ import java.util.List;
 
 public class App {
   public static void main(String[] args) throws Exception {
+    Garage garage = createDemoCars();
+
+    displayGarageIngo(garage);
+  }
+
+  public static Garage createDemoCars() {
     List<Car> cars = new ArrayList<Car>();
     CarFactory carFactory = new CarFactory();
-    GaragePrinter garagePrinter = new GaragePrinter();
 
     for (int i = 0; i <= 10; i++) {
       cars.add(carFactory.generateCar());
     }
 
-    Garage garage = new Garage(cars);
+    return new Garage(cars);
+  }
+
+  public static void displayGarageIngo(Garage garage) {
+    GaragePrinter garagePrinter = new GaragePrinter();
     garagePrinter.printCars(garage);
-
-    // System.out.println(garage.getCars().get(0).getEngineStatus());
-    // garage.getCars().get(0).startEngine();
-    // System.out.println(garage.getCars().get(0).getEngineStatus());
-    // garage.getCars().get(0).accelerate();
-    // System.out.println(garage.getCars().get(0).getSpeed());
-
-    // System.out.println(Math.sqrt(garage.getCarCount()));
-
-    // garage.removeCar(garage.getCars().get(1));
-
-    // garage.getCars().forEach((car) -> car.startEngine());
-
-    // System.out.println("Total Tyres: " + garage.getTotalTyres());
   }
 }
